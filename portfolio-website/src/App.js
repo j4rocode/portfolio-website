a440255868aa26f95194b9b106d9889597a42c89
j4rocode <piotr.jarosz10@gmail.com>
2022-09-08 08:58:11 +0200

    Update App.js

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Strona w Budowie
         </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/FutboluManiak"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Twitter
         </a>
       </header>
     </div>
